759e0eebfb3a6e0cc3a64f351a2554080233976e
Florian Schroedl <hello@florianschroedl.com>
2018-11-26 20:42:19 +0100

    Fix pipe executed function

--- src/slack-discord.js
+++ src/slack-discord.js
@@ -1,16 +1,16 @@
 const R = require('ramda');
 
-const getUnreadNodes = R.cond([
-  [R.equals   ('discordapp.com'), () => document.querySelector('[class^="nameUnreadText"]')],
-  [R.contains ('slack.com'),      () => document.querySelector('.p-channel_sidebar__channel--unread')],
-  [R.T,                           () => 'No such page']
-]);
-
-const main = pipe(
-  getUnreadNodes(document.location.host),
+const main = R.pipe(
+  R.cond([
+    [R.equals   ('discordapp.com'), () => document.querySelector('[class^="nameUnreadText"]')],
+    [R.contains ('slack.com'),      () => document.querySelector('.p-channel_sidebar__channel--unread')],
+    [R.T,                           () => 'No such page']
+  ]),
   R.ifElse(
     x => typeof x === 'object',
     R.tap(x => x.click()),
     R.tap(x => console.log('No new items found')),
   ),
-)
+);
+
+main(document.location.host);
