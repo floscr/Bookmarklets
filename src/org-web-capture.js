e2c6a273e35cafe680268085d5077a1627bc945c
Florian Schroedl <flo.schroedl@gmail.com>
2020-01-05 10:27:23 +0100

    Go back after capture

--- src/org-web-capture.js
+++ src/org-web-capture.js
@@ -1,7 +1,7 @@
 const R = require('ramda');
 
 const main = R.pipe(
-  doc => `url=${encodeURIComponent(doc.location.href)}&title=${encodeURIComponent(doc.title)}`,
+  doc => `url=${encodeURIComponent(doc.location.href)}&title=${encodeURIComponent(doc.title)}&action=go_back`,
   query => `${process.env.DOMAIN}?${query}`,
   R.tap(console.log),
   R.tap(url => window.location.href = url),
