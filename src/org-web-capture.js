162007d2f5e319675483ae778309d8ddffce25e4
Florian Schroedl <flo.schroedl@gmail.com>
2020-01-05 10:44:23 +0100

    Rename DOMAIN -> CAPTURE_DOMAIN

--- src/org-web-capture.js
+++ src/org-web-capture.js
@@ -2,7 +2,7 @@ const R = require('ramda');
 
 const main = R.pipe(
   doc => `url=${encodeURIComponent(doc.location.href)}&title=${encodeURIComponent(doc.title)}&action=go_back`,
-  query => `${process.env.DOMAIN}?${query}`,
+  query => `${process.env.CAPTURE_DOMAIN}?${query}`,
   R.tap(console.log),
   R.tap(url => window.location.href = url),
 );
